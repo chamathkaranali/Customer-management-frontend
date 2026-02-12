467bbd2fdc857cae53bd6b6c3035e92048bcc3d0
Chamathka Ranali <ranalichamathka@gmail.com>
2026-02-12 11:43:32 +0530

    feat: add new pages and routing for About, Contact, Home, and Profile
    - Implemented routing for About, Contact, Home, and Profile components in app.routes.ts.
    - Created About component with its HTML, CSS, and spec files.
    - Created Contact component with its HTML, CSS, and spec files.
    - Created Home component with its HTML, CSS, and spec files.
    - Created Profile component with its HTML, CSS, and spec files.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { About } from './page/about/about';
+import { Contact } from './page/contact/contact';
+import { Home } from './page/home/home';
+import { Profile } from './page/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"about",
+        component:About
+    },
+    {
+        path:"Contact",
+        component:Contact
+    },
+    {
+        path:"Home",
+        component:Home
+    },
+    {
+        path:"profile",
+        component:Profile
+    }
+];
